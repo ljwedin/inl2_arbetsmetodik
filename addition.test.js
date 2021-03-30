83a4c3cc9fe5b989368428ae460bd1fc63774ff1
ljwedin <ljwedin@gmail.com>
2021-03-30 08:38:55 +0200

    Expands addition test, creates addition function that passes tests

--- addition.test.js
+++ addition.test.js
@@ -4,3 +4,11 @@ const addition = require('./addition');
 test('Adds 1 + 2 to equal 3', () => {
     expect(addition(1, 2)).toBe(3);
 });
+
+test('Adds 2 + 3 to equal 5', () => {
+    expect(addition(2, 3)).toBe(5);
+});
+
+test('Adds -2 and 3 to equal 1', () => {
+    expect(addition(-2, 3)).toBe(1);
+});
